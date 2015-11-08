fb93b6b9e8dd8a55197faa3a290b636042db3181
cternes <github@slackspace.de>
2015-11-08 20:56:26 +0100

    Implemented hours

--- src/main/java/de/slackspace/tinkerclock/logic/TimeLedProvider.java
+++ src/main/java/de/slackspace/tinkerclock/logic/TimeLedProvider.java
@@ -4,6 +4,7 @@ import java.time.Clock;
 import java.time.Instant;
 import java.time.LocalTime;
 import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -14,6 +15,8 @@ import de.slackspace.tinkerclock.device.Led;
 public class TimeLedProvider {
 
 	private static final ZoneId TIMEZONE = ZoneId.of("Europe/Berlin");
+	private static String COLOR = "#ffffff";
+	
 	private Clock clock = Clock.systemDefaultZone();
 	
 	protected void setClock(Clock clock) {
@@ -21,12 +24,145 @@ public class TimeLedProvider {
 	}
 	
 	public List<Led> getTimeLeds() {
+		List<Led> leds = new ArrayList<Led>();
+		
 		Instant now = Instant.now(clock);
 		LocalTime time = LocalTime.from(now.atZone(TIMEZONE));
+
+		int hours = time.getHour(); 
 		
-		System.out.println(time.getHour());
+		if(hours >= 12) {
+			hours = hours - 12;
+		}
 		
-		return null;
+		if((hours == 0) && time.getMinute() == 0) {
+			addItIs(leds);
+			addTwelve(leds);
+			addClockWord(leds);
+		}
+		else if(hours == 1 && time.getMinute() == 0) {
+			addItIs(leds);
+			addOne(leds);
+			addClockWord(leds);
+		}
+		else if(hours == 2 && time.getMinute() == 0) {
+			addItIs(leds);
+			addTwo(leds);
+			addClockWord(leds);
+		}
+		else if(hours == 3 && time.getMinute() == 0) {
+			addItIs(leds);
+			addThree(leds);
+			addClockWord(leds);
+		}
+		else if(hours == 4 && time.getMinute() == 0) {
+			addItIs(leds);
+			addFour(leds);
+			addClockWord(leds);
+		}
+		else if(hours == 5 && time.getMinute() == 0) {
+			addItIs(leds);
+			addFive(leds);
+			addClockWord(leds);
+		}
+		else if(hours == 6 && time.getMinute() == 0) {
+			addItIs(leds);
+			addSix(leds);
+			addClockWord(leds);
+		}
+		else if(hours == 7 && time.getMinute() == 0) {
+			addItIs(leds);
+			addSeven(leds);
+			addClockWord(leds);
+		}
+		else if(hours == 8 && time.getMinute() == 0) {
+			addItIs(leds);
+			addEight(leds);
+			addClockWord(leds);
+		}
+		else if(hours == 9 && time.getMinute() == 0) {
+			addItIs(leds);
+			addNine(leds);
+			addClockWord(leds);
+		}
+		else if(hours == 10 && time.getMinute() == 0) {
+			addItIs(leds);
+			addTen(leds);
+			addClockWord(leds);
+		}
+		else if(hours == 11 && time.getMinute() == 0) {
+			addItIs(leds);
+			addEleven(leds);
+			addClockWord(leds);
+		}
+		
+		return leds;
+	}
+	
+	private void addEleven(List<Led> leds) {
+		addLeds(leds, 61, 60, 59);
+	}
+	
+	private void addTen(List<Led> leds) {
+		addLeds(leds, 1, 2, 3, 4);
+	}
+	
+	private void addNine(List<Led> leds) {
+		addLeds(leds, 4, 5, 6, 7);
+	}
+
+	private void addEight(List<Led> leds) {
+		addLeds(leds, 30, 31, 32, 33);
+	}
+
+	private void addSeven(List<Led> leds) {
+		addLeds(leds, 22, 21, 20, 19, 18, 17);
+	}
+
+	private void addSix(List<Led> leds) {
+		addLeds(leds, 23, 24, 25, 26, 27);
+	}
+
+	private void addFive(List<Led> leds) {
+		addLeds(leds, 59, 58, 57, 56);
+	}
+
+	private void addFour(List<Led> leds) {
+		addLeds(leds, 37, 36, 35, 34);
+	}
+
+	private void addThree(List<Led> leds) {
+		addLeds(leds, 44, 43, 42, 41);
+	}
+
+	private void addTwo(List<Led> leds) {
+		addLeds(leds, 52, 53, 54, 55);
+	}
+
+	private void addOne(List<Led> leds) {
+		addLeds(leds, 45, 46, 47);
+	}
+
+	private void addClockWord(List<Led> leds) {
+		addLeds(leds, 9, 10, 11);
+	}
+
+	private void addItIs(List<Led> leds) {
+		// it
+		addLeds(leds, 109, 110);
+		
+		// is
+		addLeds(leds, 105, 106, 107);
+	}
+	
+	private void addTwelve(List<Led> leds) {
+		addLeds(leds, 16, 15, 14, 13, 12);
+	}
+	
+	private void addLeds(List<Led> leds, int... indices) {
+		for (int i : indices) {
+			leds.add(new Led(i, COLOR));
+		}
 	}
 	
 }
--- src/test/java/de/slackspace/tinkerclock/logic/TimeLedProviderTest.java
+++ src/test/java/de/slackspace/tinkerclock/logic/TimeLedProviderTest.java
@@ -1,7 +1,6 @@
 package de.slackspace.tinkerclock.logic;
 
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertFalse;
 
 import java.time.Clock;
 import java.time.Instant;
@@ -20,15 +19,89 @@ public class TimeLedProviderTest {
 	
 	@Test
 	public void shouldReturnLedsForNoon() {
-		setTime(12, 0);
+		checkFullHour(12, 16, 15, 14, 13, 12);
+	}
+	
+	@Test
+	public void shouldReturnLedsForOnePm() {
+		checkFullHour(13, 45, 46, 47);
+	}
+	
+	@Test
+	public void shouldReturnLedsForOneAm() {
+		checkFullHour(1, 45, 46, 47);
+	}
+	
+	@Test
+	public void shouldReturnLedsForTwoPm() {
+		checkFullHour(14, 52, 53, 54, 55);
+	}
+	
+	@Test
+	public void shouldReturnLedsForThreePm() {
+		checkFullHour(15, 44, 43, 42);
+	}
+	
+	@Test
+	public void shouldReturnLedsForFourPm() {
+		checkFullHour(16, 37, 36, 35, 34);
+	}
+	
+	@Test
+	public void shouldReturnLedsForFivePm() {
+		checkFullHour(17, 59, 58, 57, 56);
+	}
+	
+	@Test
+	public void shouldReturnLedsForSixPm() {
+		checkFullHour(18, 23, 24, 25, 26, 27);
+	}
+	
+	@Test
+	public void shouldReturnLedsForSevenPm() {
+		checkFullHour(19, 22, 21, 20, 19, 18, 17);
+	}
+	
+	@Test
+	public void shouldReturnLedsForEightPm() {
+		checkFullHour(20, 30, 31, 32, 33);
+	}
+	
+	@Test
+	public void shouldReturnLedsForNinePm() {
+		checkFullHour(21, 4, 5, 6, 7);
+	}
+	
+	@Test
+	public void shouldReturnLedsForTenPm() {
+		checkFullHour(22, 1, 2, 3, 4);
+	}
+	
+	@Test
+	public void shouldReturnLedsForElevenPm() {
+		checkFullHour(23, 61, 60, 59);
+	}
+	
+	@Test
+	public void shouldReturnLedsForMidnight() {
+		checkFullHour(0, 16, 15, 14, 13, 12);
+	}
+	
+	private void checkFullHour(int hour, int... indices) {
+		setTime(hour, 0);
 		
 		List<Led> leds = cut.getTimeLeds();
-		assertThat(leds, hasSize(2));
-		
-		//1
-		//2
-		//4,5,6
-		
+		assertThatLedsIncludeItIs(leds);
+		assertThatLedsIncluded(leds, indices);
+		assertThatLedsIncludeClockWord(leds);
+	}
+	
+	private void assertThatLedsIncludeItIs(List<Led> leds) {
+		assertThatLedsIncluded(leds, 110, 109, 107, 106, 105);
+	}
+	
+	private void assertThatLedsIncludeClockWord(List<Led> leds) {
+		assertThatLedsIncluded(leds, 9, 10, 11);
 	}
 
 	private void setTime(int hours, int minutes) {
@@ -37,5 +110,20 @@ public class TimeLedProviderTest {
 		cut.setClock(Clock.fixed(fixedInstant, TIMEZONE));
 	}
 	
+	private void assertThatLedsHaveIndex(List<Led> leds, int expectedIndex) {
+		for (Led led : leds) {
+			if(led.getIndex() == expectedIndex) {
+				return;
+			}
+		}
+		
+		assertFalse("Expected index '" + expectedIndex + "' was not found", true);
+	}
+	
+	private void assertThatLedsIncluded(List<Led> leds, int... expectedIndices) {
+		for (int index : expectedIndices) {
+			assertThatLedsHaveIndex(leds, index);
+		}
+	}
 	
 }
